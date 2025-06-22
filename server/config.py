26506c44a71f7f42f2686881b23a6590e717985c
ian ayiro <ian.ayiro@student.moringa.com>
2025-06-22 17:57:11 +0300

    switched from SQL to postgres

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///late_show.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://late_show_user:strongpa5S@localhost/late_show')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback-jwt-secret')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
